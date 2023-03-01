3c2ef1eaa3b321f7a5e40d486f43200db409d5d9
MalwinaS <malwinastepien93@gmail.com>
2023-03-01 18:41:29 +0100

    create vue app

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Lista to do</h1>
+    <TodoApp />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import todo from './components/TodoApp.vue'
+  export default {
+    components: {
+      TodoApp : todo
+    }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
