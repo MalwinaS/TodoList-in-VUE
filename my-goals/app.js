bd91a7c382bf762b6a614dc88a73ca067e545e37
MalwinaS <malwinastepien93@gmail.com>
2023-03-13 17:13:53 +0100

    task

---
+++ my-goals/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      goalA: 'Finish the course and learn Vue!',
+      goalB: 'Master Vue and build amazing apps!',
+      vueLink: 'https://vuejs.org/',
+    };
+  },
+  methods: {
+    outputGoal() {
+        const randomNumber = Math.random()
+        if(randomNumber < 0.5) {
+            return this.goalA
+        } else {
+            return this.goalB 
+        }
+    }
+  }
+});
+
+app.mount("#user-goal");
+  
